06d20b26705997fbe617c7107b88505d5de7e956
カグチ <59391215+KagChi@users.noreply.github.com>
2021-07-26 10:31:26 +0700

    chore(youtube): options for load full track

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const YoutubeSource = require("./sources/youtube")
 const IDRegex = /(ytsearch:)?(.+)/;
 module.exports = class trackResolver {
+    constructor(options) {
+        this.options = options
+    }
     //some code copied from volcano: https://github.com/AmandaDiscord/Volcano
 	async load(query) {
 		const payload = {
@@ -14,16 +17,15 @@ module.exports = class trackResolver {
 		let url;
 		if (query.startsWith("http")) url = new URL(query);
 		if(isYouTubeSearch || (url && url.hostname.includes("youtube"))) {
-			const data = await YoutubeSource(resource || query, isYouTubeSearch);
-		
+			const data = await YoutubeSource(resource || query, isYouTubeSearch, this.options.loadFullPlaylist);
 			if (!data.entries) return;
 			const infos = data.entries.map(i => ({ identifier: i.id, author: i.channel.name, length: Math.round(i.duration * 1000), isStream: i.isLive || i.isLiveContent, isSeekable: i.duration !== 0, position: 0, uri: `https://youtube.com/watch?v=${i.id}`, sourceName: "youtube" }));
-		  if (data.plData) {
-		  	payload.playlistInfo = data.plData;
-		  	playlist = true;
-		  }
-		  payload.tracks = infos;
-		}
+            if (data.plData) {
+                payload.playlistInfo = data.plData;
+                playlist = true;
+            }
+            payload.tracks = infos;
+        }
 		return Object.assign({ loadType: payload.tracks.length > 1 && isYouTubeSearch ? "SEARCH_RESULT" : playlist ? "PLAYLIST_LOADED" : "TRACK_LOADED" }, payload )
 	}
 }
--- src/sources/youtube.js
+++ src/sources/youtube.js
@@ -1,6 +1,6 @@
 const { Client } = require("youtubei");
 const youtube = new Client();
-module.exports = async function youtubeSource(query, isSearch) {
+module.exports = async function youtubeSource(query, { isSearch, loadAllTrack }) {
     //some code copied from volcano: https://github.com/AmandaDiscord/Volcano
 	if(isSearch) {
 		const videos = await youtube.search(query, { type: "video" });
@@ -11,7 +11,7 @@ module.exports = async function youtubeSource(query, isSearch) {
 	if (url && url.searchParams.get("list") && url.searchParams.get("list").startsWith("FL_") || query.startsWith("FL_")) throw new Error("Favorite list playlists cannot be fetched.");
 	if (url && url.searchParams.has("list") || query.startsWith("PL")) {
 		const data = await youtube.getPlaylist(query);
-		await data.next(0)
+		if(loadAllTrack) await data.next(0)
 		return { entries: data.videos, plData: { name: data.title, selectedTrack: url.searchParams.get("index") ? Number(url.searchParams.get("index")) : 1 } };
 	}
 	const data = await youtube.getVideo(query);
